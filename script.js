530294e2ac765650f536d0df4a6b844ef1652aae
Brandon Moreno <brandonm99@me.com>
2022-09-11 22:42:17 -0600

    pizza style

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Welcome to the Brandon's dimension";
 
 document.querySelector("body").appendChild(h2);
